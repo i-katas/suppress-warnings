4d8f5727893d0960c7ded26fc2a99619907b1363
i-katas <feexonj@gmail.com>
2020-03-05 13:11:26 +0800

    make arity to be used standalone in browser

--- src/arity.js
+++ src/arity.js
@@ -1,27 +1,29 @@
-module.exports = function arity(n, callback) {
-  var make = cache[n] || (cache[n] = factory(n));
-  return arguments.length > 1 ? make(callback) : make;
-}
+(function(define) {
+  var cache = {};
+  var supportsRewriteLength = (function(rewrite){ 
+    try {
+      return rewrite && rewrite(arguments.callee, 'length', {value: 5}).length == 5; 
+    } catch(e) {
+      return false;
+    }
+  })(Object.defineProperty);
 
-var cache = {};
-var supportsRewriteLength = (function(define){ 
-  try {
-    return define && define(arguments.callee, 'length', {value: 5}).length == 5; 
-  } catch(e) {
-    return false;
+  function factory(n) {
+    if(supportsRewriteLength) {
+      return function(f) {
+        return Object.defineProperty(f, 'length', {value: n});
+      };
+    }
+    return new Function("f", 'return function('+withArgs(n)+'){return f.apply(this, arguments)}');
   }
-})(Object.defineProperty);
 
-function factory(n) {
-  if(supportsRewriteLength) {
-    return function(f) {
-      return Object.defineProperty(f, 'length', {value: n});
-    };
+  function withArgs(n) {
+      return Array(n).fill('_').join(',');
   }
-  return new Function("f", 'return function('+withArgs(n)+'){return f.apply(this, arguments)}');
-}
 
-function withArgs(n) {
-    return Array(n).fill('_').join(',');
-}
+  define('arity', function (n, callback) {
+    var make = cache[n] || (cache[n] = factory(n));
+    return arguments.length > 1 ? make(callback) : make;
+  })
 
+})(typeof module == 'object' && typeof module.exports == 'object' ?  function(_, fn) { module.exports = fn } : function(name, fn) { window[name] = fn})
