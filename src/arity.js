692b7b37f7e804051da66c63e45d0ca2f590440d
i-katas <feexonj@gmail.com>
2020-03-05 12:43:30 +0800

    optimize arity if current environment supports rewrite the length of the function

--- src/arity.js
+++ src/arity.js
@@ -1,12 +1,24 @@
 module.exports = function arity(n, callback) {
-  var make = factory(n);
+  var make = cache[n] || (cache[n] = factory(n));
   return arguments.length > 1 ? make(callback) : make;
 }
 
 var cache = {};
+var supportsRewriteLength = (function(define){ 
+  try {
+    return define && define(arguments.callee, 'length', {value: 5}).length == 5; 
+  } catch(e) {
+    return false;
+  }
+})(Object.defineProperty);
 
 function factory(n) {
-  return cache[n] || (cache[n] = new Function("f", 'return function('+withArgs(n)+'){return f.apply(this, arguments)}'));
+  if(supportsRewriteLength) {
+    return function(f) {
+      return Object.defineProperty(f, 'length', {value: n});
+    };
+  }
+  return new Function("f", 'return function('+withArgs(n)+'){return f.apply(this, arguments)}');
 }
 
 function withArgs(n) {
